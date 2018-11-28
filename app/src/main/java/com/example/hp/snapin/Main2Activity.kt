f41b0259fca79f755eb30d4f68c2c0599265c3bd
areeshaFatima <areeshaf7@gmail.com>
2018-11-28 17:22:33 +0530

    logout

--- app/src/main/java/com/example/hp/snapin/Main2Activity.kt
+++ app/src/main/java/com/example/hp/snapin/Main2Activity.kt
@@ -2,11 +2,40 @@ package com.example.hp.snapin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
 class Main2Activity : AppCompatActivity() {
 
+    val mAuth=FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflator=menuInflater
+        inflator.inflate(R.menu.snaps,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if(item?.itemId==R.id.newsnap){
+
+        }else if(item?.itemId==R.id.logout){
+            mAuth.signOut()
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        mAuth.signOut()
     }
 }
