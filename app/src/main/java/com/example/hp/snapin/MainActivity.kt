8feb9c386b6c4e95e606bd0a071de67d8b20a7da
areeshaFatima <areeshaf7@gmail.com>
2018-11-28 13:29:15 +0530

    login complete

--- app/src/main/java/com/example/hp/snapin/MainActivity.kt
+++ app/src/main/java/com/example/hp/snapin/MainActivity.kt
@@ -4,20 +4,64 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import android.R.attr.password
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
 
     var emailEditText: EditText?=null
     var passwordEditText: EditText?=null
+
+    val mAuth=FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         emailEditText=findViewById(R.id.emailEditText)
         passwordEditText=findViewById(R.id.passwordEditText)
+
+        if (mAuth.currentUser!=null){
+            login()
+        }
+
+
     }
 
+    fun login(){
+
+        //move to next activity
+        val intent=Intent(this,Main2Activity::class.java)
+        startActivity(intent)
+    }
 
     fun loginClicked(view : View){
 
+        //check in the user in not logged in or sign up new user
+        mAuth.signInWithEmailAndPassword(emailEditText?.text.toString(), passwordEditText?.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    login()
+                } else {
+                    mAuth.createUserWithEmailAndPassword(emailEditText?.text.toString(), passwordEditText?.text.toString())
+                        .addOnCompleteListener(this){
+                                task ->
+                            if(task.isSuccessful){
+                                //add to database
+                                login()
+                            }else{
+                                Toast.makeText(this,"Login Failed! Try again.",Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                }
+
+            }
     }
 }
