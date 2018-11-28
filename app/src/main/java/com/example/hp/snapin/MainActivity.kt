74d9d4c7da8ebd53004ca6371c8c3d70efabbf58
areeshaFatima <areeshaf7@gmail.com>
2018-11-28 12:38:42 +0530

    login page

--- app/src/main/java/com/example/hp/snapin/MainActivity.kt
+++ app/src/main/java/com/example/hp/snapin/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.hp.snapin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
+    var emailEditText: EditText?=null
+    var passwordEditText: EditText?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        emailEditText=findViewById(R.id.emailEditText)
+        passwordEditText=findViewById(R.id.passwordEditText)
+    }
+
+
+    fun loginClicked(view : View){
+
     }
 }
